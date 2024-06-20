e945f730ce221b7e6beae21ae98411b7bdf5698b
Jameson Blake Collins <jblakecollins84@gmail.com>
2024-06-20 16:49:05 -0500

    Seeting up create TODO: handle client closed error

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -1,10 +1,23 @@
-'use server'
+"use server";
 
-// import { client } from "@/lib/db"
-import { redirect } from 'next/navigation'
+import { client } from "@/lib/db";
+import { redirect } from "next/navigation";
 
 export async function createBook(formData) {
-  const {title, rating, author, blurb} = Object.fromEntries(formData)
+  if (!client.isOpen) {
+    console.log(
+      "\n!!!!!!!!!!!!!! Redis client is not connected !!!!!!!!!!!!!!\n"
+    );
+    return { error: "Redis client is not connected" };
+  }
 
-  
+  const { title, rating, author, blurb } = Object.fromEntries(formData);
+
+  // create book id
+  const id = Math.floor(Math.random() * 1000000); // don't use in prod
+
+  // save new hash for the book
+  await client.hSet(`book:${id}`, { title, rating, author, blurb });
+
+  redirect("/");
 }
